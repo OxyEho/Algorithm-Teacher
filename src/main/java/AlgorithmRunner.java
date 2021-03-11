9b31c9981f6f520065d4da364d45c9ae5e1fc0f9
OxyEho <valera808@inbox.ru>
2021-03-11 17:05:39 +0500

    add graph

--- src/main/java/AlgorithmRunner.java
+++ src/main/java/AlgorithmRunner.java
@@ -1,3 +1,22 @@
-public class AlgorithmRunner {
+import java.util.*;
+import java.util.concurrent.Callable;
 
+
+public class AlgorithmRunner {
+    private static Graph<Integer> exampleGraph;
+    private AlgorithmRunner(){
+        HashMap<Integer, ArrayList<Integer>> originMap = new HashMap<>(){
+            {
+                put(1, new ArrayList<>(Arrays.asList(2, 3, 4)));
+                put(2, new ArrayList<>(Collections.singletonList(1)));
+                put(3, new ArrayList<>(Arrays.asList(1, 4)));
+                put(4, new ArrayList<>(Arrays.asList(1, 3, 5)));
+                put(5, new ArrayList<>(Collections.singletonList(4)));
+            }
+        };
+        exampleGraph = new Graph<>(originMap);
+    }
+    public static void runAlgorithm(Callable<ArrayList<ArrayList<Integer>>> func){
+
+    }
 }
